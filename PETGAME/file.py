029002c4ebeb962bfb0f41c4fb6c3ea8727336e8
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-10-23 17:29:53 -0300

    [FIX][PETGAME]
    nnao consegui transformar em exe, depois tentar de novo

--- PETGAME/file.py
+++ PETGAME/file.py
@@ -7,6 +7,8 @@ diretorio_images = os.path.join(diretorio_assets, 'image')
 diretorio_musics = os.path.join(diretorio_assets, 'musics')
 diretorio_sprites = os.path.join(diretorio_assets, 'sprites')
 
+# diretorios a serem exportados
+
 diretorio_gato = os.path.join(diretorio_sprites, 'gato')
 diretorio_passaro = os.path.join(diretorio_sprites, 'passaro')
 diretorio_patolion = os.path.join(diretorio_sprites, 'patolion')
